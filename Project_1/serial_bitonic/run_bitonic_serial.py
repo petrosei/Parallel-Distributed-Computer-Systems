9f35f9e49843953bf7593cc5485c9fc770b5a257
Despar <despargy@ece.auth.gr>
2017-10-22 19:13:21 +0300

    python script modified

--- Project_1/serial_bitonic/run_bitonic_serial.py
+++ Project_1/serial_bitonic/run_bitonic_serial.py
@@ -11,19 +11,18 @@ import os
 DEVNULL = open(os.devnull, 'wb')
 
 processors = [1,2, 4, 8, 16, 32, 64, 128, 256]
-Q = range(12, 12)
+Q = range(12, 16)
 
 
+command = "./bitonic_serial"
 
-p1 = Popen(["mkdir","../Results/serial_bitonic_results"], stdout=PIPE)
-'p1out = p1.communicate()'
-p2 = Popen(["rm","../Results/serial_bitonic_results/* >dev/null"], stdout=PIPE)
-'p2out = p2.communicate()'
+pmk1 = Popen(["mkdir","../Results"], stdout=DEVNULL, stderr=DEVNULL)
+p1 = Popen(["mkdir","../Results/serial_bitonic_results"], stdout=DEVNULL,stderr=DEVNULL)
+p2 = Popen(["rm","../Results/serial_bitonic_results/*"], stdout=DEVNULL,stderr=DEVNULL)
+
 for q in Q:
-	filename = "bsortresults{0}.txt".format(q)
-	command = "./serial_bitonic"
-	arguments = "{0}",format(q)
-	p = subprocess.Popen([command,arguments], stdout=subprocess.PIPE)
-	pout = p.communicate()[0]
-        print  pout
-
+#	filename = "bsortresults{}.txt".format(q)
+	arguments = "{}".format(q)
+	p = Popen([command,arguments], stdout=PIPE)
+       	output = p.communicate()[0]
+	print output
